d81044a549ec09a52790b988fc8e75d82ac4c5c3
unknown <rgawron@.(none)>
2011-01-06 17:42:04 +0100

    files for post about storing image as HTML table added

---
+++ image2table/img.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import string
+import Image
+
+def save_image(image, template, output_file):
+    data = string.replace(template, 'IMAGE', image)
+    output = open(output_file, 'w')
+    output.write(data)
+
+def parse_image(image):
+    buffer = '<table>'
+    for j in range(image.size[1]):
+        buffer += '<tr>'
+        for i in range(image.size[0]):
+            color = "rgb(%d, %d, %d)" % image.getpixel((i, j))
+            buffer += "<td style=\"background:%s\"></td>" % color
+        buffer += '</tr>'
+    return buffer + '</table>'
+
+if __name__=="__main__":
+    image_name = sys.argv[1]
+    template = open('template.html', 'r').read()
+    image = Image.open(image_name).convert('RGB')
+    save_image(parse_image(image), template, 'output.html')
