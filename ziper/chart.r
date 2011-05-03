db41f982b148373fbbf5231fc665623df28717d8
RobertGawron <robert_gawron@yahoo.fr>
2011-05-03 13:18:37 +0200

    back to the begining, minor changes involved

--- ziper/chart.r
+++ ziper/chart.r
@@ -2,10 +2,10 @@ args<-commandArgs(TRUE)
 sizes <- scan(args[1])
 image_name <- paste( "./chart", args[1], ".png")
 
-png(filename=image_name, height=700, bg="white")
+png(filename=image_name, width=500, height=700, bg="white")
 
 barplot(sizes, main=args[2], xlab="iterations", 
     ylab="size of zip archive in bytes", xpd=F, col="grey",
-    ylim=round(c(min(sizes), max(sizes)), 2))
+    ylim=(c(min(sizes), max(sizes))))
 
 axis(2, at=c(min(sizes), max(sizes)))
