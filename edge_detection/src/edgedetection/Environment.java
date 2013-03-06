6db40e0776b3d47a8366839d51caf61dfce1e720
RobertGawron <robert_gawron@yahoo.fr>
2013-03-06 19:00:22 +0100

    final version

--- edge_detection/src/edgedetection/Environment.java
+++ edge_detection/src/edgedetection/Environment.java
@@ -4,15 +4,25 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 import java.awt.*;
-import java.awt.Color.*;
-import java.util.ArrayList;
-import java.awt.Point;
-//import edgedetection.poupulation;
+import java.util.*;
 
 // it's imposible to inherit from ImageIO
 public class Environment {
+
     public Environment(String inputFileName) throws IOException {
         terrain = ImageIO.read(new File(inputFileName));
+        // blur the image
+        float[] blurKernel = {
+              0f, 1/9f,   0f,
+            1/9f, 5/9f, 1/9f,
+              0f, 1/9f,   0f
+        };
+
+        BufferedImageOp blur = new ConvolveOp(new Kernel(3, 3, blurKernel));
+        terrain = blur.filter(terrain, new BufferedImage(terrain.getWidth(),
+            terrain.getHeight(),terrain.getType()));
+        
+        
         // convert image to greyscale
         BufferedImage tmp = new BufferedImage(
                                 terrain.getWidth(), 
@@ -21,9 +31,33 @@ public class Environment {
     
         Graphics2D graphics = tmp.createGraphics();
         graphics.drawImage(terrain, 0, 0, null);
-        terrain = tmp;  
+        terrain = tmp;
+
+        // init random generator
+        oracle = new Random();
+        
+        // a dummy initial amount of specimens
+        maxPopulationSize = width() * height();
+        specimens = new HashSet<Specimen>();
+        
+        for (int i = 0; i < maxPopulationSize; i++) {
+            Point birthPlace = new Point(oracle.nextInt(width()), oracle.nextInt(height())); 
+            Specimen child = new Specimen(birthPlace);
+            if (getFitness(child)){
+                specimens.add(child);        
+            }
+        }
     }
 
+    public HashSet<Specimen> evolve(int generations) {
+        for (int i = 0; i < generations; i++) {
+            rebuild();
+            //System.out.println("iteration: " + i + ", " + specimens.size() + " speciments");
+            System.out.println(specimens.size());
+        }        
+        return specimens;    
+    }
+        
     public BufferedImage asImage() {
         return terrain;
     }
@@ -35,70 +69,51 @@ public class Environment {
     public int height(){
         return terrain.getHeight();
     }
-    
-    
-/*    public void addPopulation(Population population) {
-        this.populations.add(population);
-    }*/
-    /*    
-    public BufferedImage pokazTeren(Color figury, Color tlo){
-        // zmienmy kolory na obrazku
-        for(int i=0; i<terrain.getWidth(); i++){
-            for(int j=0; j<terrain.getHeight(); j++){
-                Color c = new Color( terrain.getRGB(i,j) );
-                if(getGrey(c)==0)
-                    terrain.setRGB(i,j, figury.getRGB());
-                else
-                    terrain.setRGB(i,j, tlo.getRGB());
-            }
-        }
 
-        return terrain;
-    }
-    
-    // 1-bialy, 0 czarny
-    public int getSytuacja(Point p){
-        Color c = new Color( this.terrain.getRGB(p.x, p.y) );
-        return 1 - this.getGrey(c)/255;
-    }
-    
-    
-    // pola i metody prywatne
-    private int getGrey(Color c){
-        return ( c.getGreen()+c.getRed()+c.getBlue() )/3;
-    }
-    
-    private void makeItBlackOrWhite(){
-        for(int i=0; i<terrain.getWidth(); i++){
-            for(int j=0; j<terrain.getHeight(); j++){
-               int val = terrain.getRGB(i,j);
-               Color c = new Color(val);
-               int grey = getGrey(c);
-               if(grey < blackWhiteEdge)
-                    terrain.setRGB(i,j, 0x000000);
-                else
-                    terrain.setRGB(i,j, 0xffffff);
+    private void rebuild() {
+        HashSet<Specimen> newGeneration = new HashSet<Specimen>();
+        for(Specimen mother : specimens) {
+            Specimen child = mother.bornChild();
+            
+            if (mother.place() !=  child.place() && getFitness(child))
+            {
+                newGeneration.add(child);
             }
         }
+        
+        specimens.addAll(newGeneration);
     }
-    
-    private void makeItGrey(){
-        for(int i=0; i<terrain.getWidth(); i++){
-            for(int j=0; j<terrain.getHeight(); j++){
-                int val = terrain.getRGB(i,j);
-                Color c = new Color(val);
-                int grey = (c.getGreen()+c.getRed()+c.getBlue())/3;
-                //int grey = i+j;
-                // bialy to 0xffffff, czarny to 0x0
-                // grey ma zakres 0..255, skalujemy:
-                double tmp = (double)grey%(double)255;
-                grey = (int)((double)0xffffff * tmp);
-                terrain.setRGB(i,j, grey);
-                
-            }
+
+    private boolean getFitness(Specimen examined) {
+        int x = examined.place().x;
+        int y = examined.place().y;               
+ 
+        // he can't live out of the terrain anyway 
+        if ( !(x >= 0 && x < width() && y >= 0 && y < height()) ) {
+            return false;
         }
-    }
-*/        
+        
+        int neighborhood = 5;
+        int goodNeighbours = 0;
+        
+        for (int i = 0;  i < neighborhood; i++) {
+            for (int j = 0;  j < neighborhood; j++) {
+                Point p = new Point(x + i, y + j);
+                if ((p.x > -1 && p.x < width()) 
+                        && (p.y > -1 && p.y < height())
+                        && !(i == 0 && j == 0)
+                        && terrain.getRGB(x, y) == Color.WHITE.getRGB()
+                        && terrain.getRGB(p.x, p.y) == Color.WHITE.getRGB()) {
+                    goodNeighbours ++;
+                }                
+            }        
+        }
+        
+        return (goodNeighbours > 0 && goodNeighbours < 13);
+    }    
+       
     private BufferedImage terrain;
-//    private ArrayList<Population> populations;
+    private Random oracle;
+    private HashSet<Specimen> specimens; 
+    private int maxPopulationSize;
 }
--- edge_detection/src/edgedetection/Specimen.java
+++ edge_detection/src/edgedetection/Specimen.java
@@ -1,12 +1,5 @@
 package edgedetection;
 
-import java.awt.image.*;
-import java.io.*;
-import javax.imageio.*;
-import java.awt.*;
-import java.awt.image.ColorModel;
-import java.awt.image.IndexColorModel;
-import java.awt.Color.*;
 import java.awt.Point;
 import java.util.Random;
 
@@ -14,70 +7,46 @@ public class Specimen {
 
     public Specimen(Point birthPlace) {
         place = birthPlace;
+        oracle = new Random();
     }
+
+    @Override 
+    public int hashCode() {
+        return place.x << 4 & place.y;
+    }
+    
+    @Override 
+    public boolean equals(Object buddy) {
+        if (buddy == null) {
+            return false;
+        }
+        if (buddy.getClass() != this.getClass()) {
+            return false;
+        }
+        
+        Point hisPlace = ((Specimen)buddy).place;        
+        return (hisPlace.x == place.x) && (hisPlace.y == place.y);
+    }
+       
     public Point place() {
         return place;
     }
-/*    
-    public void mutuj(){
-        // zalozmy, ze mutacja jest mala, max 20%
-        int maxMutacja = 20; // w procentach
-        Random gen = new Random();
-        
-        place.x = place.x + gen.nextInt(2*maxMutacja)-maxMutacja;
-        place.y = place.y + gen.nextInt(2*maxMutacja)-maxMutacja;
-        
-    }
-    
-    public Specimen krzyzuj(Specimen t){
-        Random gen = new Random();
-        Point lokacja = new Point();
-        if(gen.nextDouble()>=0.5){
-            lokacja.x = this.place.x + t.dajPozycje().x%10;
-            lokacja.y = this.place.y + t.dajPozycje().y%10;
-        }
-        else{
-            lokacja.x = t.dajPozycje().x + this.place.x%10;
-            lokacja.y = t.dajPozycje().y + this.place.y%10;    
-        }
-        return new Specimen(lokacja);
-    }
-    
-    // interfejs: setery, getery
-    public int dajKolor(){
-        return this.kolor.getRGB();
-    }
-    
-    public Point dajPozycje(){
-        return this.place;
-    }
-    
-    public int dajDopasowanie(Terain teren){        
-         // sprawdzimy kwadrat 5x5, w srodku jest nasz osobnik, chcemy 
-         // wiedziec czy jego otoczenie zawiera krawedzie, zalozmy ze jezeli
-         // sa krawedzie to jest troche bialych i troche czarnych pixeli w okolo
-         int dopasowanie = 0;            
-         int promien = 2;
-         int x = place.x;
-         int y = place.y;
-             
-         for(int i=(x-promien); i<(x+promien); i++){
-            for(int j=(y-promien); j<(y+promien); j++){                
-                if(i<teren.getWidth() && i>=0 && j<teren.getHeight() && j>=0){
-                    dopasowanie+=teren.getSytuacja(new Point(i,j));
-                }                    
-            }
-         }
 
-         if(dopasowanie>0 && dopasowanie<Math.pow(promien, 2))
-            return 1;
-         
-         return 0;
+    public Specimen bornChild() {
+        Point birthPlace = new Point();
+        int range = 4;
+        birthPlace.x = place.x + oracle.nextInt(2 * range + 1) - range;
+        birthPlace.y = place.y + oracle.nextInt(2 * range + 1) - range;
+        
+        if (oracle.nextDouble() < mutationChance) {
+            int mutationRange = 50;
+            birthPlace.x = birthPlace.x - oracle.nextInt(2 * mutationRange + 1) - mutationRange;
+            birthPlace.y = birthPlace.y - oracle.nextInt(2 * mutationRange + 1) - mutationRange;        
+        }
+        return new Specimen(birthPlace);
     }
-    
-    // pola prywatne
-*/
-     private Point place;
-//    private Color kolor;
-
+   
+    private Point place;
+    static double mutationChance = 0.5;    
+    Random oracle;
 }
