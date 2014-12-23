852c58d2ba00894c983d078576c3cc68ac0c84d0
RobertGawron <robert_gawron@yahoo.fr>
2014-12-23 21:15:58 +0100

    First working version.

--- EmbeddedLayoutGenerator/src/layoutgenerator/LayoutGenerator.java
+++ EmbeddedLayoutGenerator/src/layoutgenerator/LayoutGenerator.java
@@ -12,39 +12,56 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
 
-
+import java.util.List;
+import java.util.ArrayList;
 
 
 class ParsedLayout {
-	public ParsedLayout(String inputFileNaname, String outputFileNaname) {
+	public ParsedLayout(String inputFileNaname, String outputFileNaname, String unitName) {
 		this.inputFileNaname = inputFileNaname;
 		this.outputFileNaname = outputFileNaname;
+		this.unitName = unitName;
 		usedPixelsAmount = 0;
 	}
 
 	public void parse() throws IOException {
+	
+		
 		System.out.println(inputFileNaname);
 		BufferedImage inputImage = ImageIO.read(new File(inputFileNaname));
 
 		PrintWriter writer = new PrintWriter(outputFileNaname, "UTF-8");
-		writer.println("int introData[][2] = {");
+		writer.println("unsigned char " + unitName + "_data[][2] = {");
 
+		
+		//List<Integer[]> myList = new ArrayList<Integer[]>();
+		//List<Integer, Integer> l
+		
 		for (int x = 0; x < inputImage.getWidth(); x++) {
+			writer.print("\t");
 			for (int y = 0; y < inputImage.getHeight(); y++) {
 				if (inputImage.getRGB(x, y) == Color.BLACK.getRGB()) {
-					writer.println("\t{" + x + ", " + y + "},");
+					writer.print("{" + x + ", " + y + "}, ");
+					//Integer[] entry = {x, y};
+					//myList.add(entry);
 					usedPixelsAmount++;
 				}
+				
 
 			}
+			writer.println("");
 		}
+		
+		
 		writer.println("};");
+		writer.println("unsigned " + unitName + "_size = " + usedPixelsAmount + ";\n");
 		writer.close();
 	}
-
+	
 	private Integer usedPixelsAmount;
 	private String inputFileNaname;
 	private String outputFileNaname;
+	private String unitName;
 }
 
 public class LayoutGenerator {
@@ -88,7 +105,7 @@ public class LayoutGenerator {
 					System.out.println(headerFileName);
 					
 					ParsedLayout layout = new ParsedLayout(
-							inputDirectory +alp.getName(), inputDirectory + headerFileName + ".h");
+							inputDirectory +alp.getName(), inputDirectory + headerFileName + ".h", headerFileName);
 					try {
 						layout.parse();
 						System.out.println("Parsing done.");
